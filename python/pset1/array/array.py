adc4fe976b6beda71a87013c8f2d83d5118ec4eb
yakiitory <steven.ebreo1973@gmail.com>
2025-09-02 18:28:48 +0800

    Implement pset1/array.py

--- python/pset1/array/array.py
+++ python/pset1/array/array.py
@@ -6,3 +6,24 @@
     Remove one student
     Print updated list
 """
+
+def main():
+    students = ["Harry", "Hermione", "Ron", "Neville", "Draco"]
+    print("Before update:")
+    for student in students:
+        print(student)
+    
+    # Inserts onto array and prints
+    students.append("Luna")
+    print("After insertion: ")
+    for student in students:
+        print(student)
+
+    # Removes a student
+    students.remove("Draco")
+    print("After deletion: ")
+    for student in students:
+        print(student)
+
+if __name__ == "__main__":
+    main()
