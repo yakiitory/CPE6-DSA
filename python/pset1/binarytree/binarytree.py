d64680f8975bcb5739d0fac8f73448855931ea6c
yakiitory <steven.ebreo1973@gmail.com>
2025-09-03 01:10:50 +0800

    Finish pset1/binarytree

--- python/pset1/binarytree/binarytree.py
+++ python/pset1/binarytree/binarytree.py
@@ -4,3 +4,27 @@
     Create a binary tree with at least 7 nodes.
     Implement inorder, preorder, and postorder traversals
 """
+
+from adt import BinaryTree
+
+def main():
+    tree = BinaryTree(50)
+    root = tree.root
+    values = [30, 70, 20, 40, 60, 80]
+    for value in values:
+        tree.insert(root, value)
+
+    preorder_list = []
+    tree.preorder(root, preorder_list)
+    print(*preorder_list, sep=", ")
+
+    inorder_list = []
+    tree.inorder(root, inorder_list)
+    print(*inorder_list, sep=", ")
+
+    postorder_list = []
+    tree.postorder(root, postorder_list)
+    print(*postorder_list, sep=", ")
+
+if __name__ == "__main__":
+    main()
