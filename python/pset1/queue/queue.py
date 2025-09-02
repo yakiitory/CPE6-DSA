35c691abb791f1edbfae0cb80207cdd225ca05c3
yakiitory <steven.ebreo1973@gmail.com>
2025-09-02 23:48:52 +0800

    Finish pset1/queue

--- python/pset1/queue/queue.py
+++ python/pset1/queue/queue.py
@@ -4,3 +4,29 @@
     Simulate a line of people waiting:
     Add 3 names then remove the first one
 """
+
+from adt import Queue
+
+def main():
+    queue = Queue()
+    print("Queue is now open!")
+    print("Current Queue: ", end="")
+
+    queue.enqueue("Steven")
+    queue.enqueue("Gea")
+    queue.enqueue("Faye")
+
+    for i in range(queue.get_len()):
+        print(queue.queue[i], end=" ")
+    print()
+
+    print(f"{queue.dequeue()} has exited the queue!")
+
+    print("New queue: ", end="")
+    for i in range(queue.get_len()):
+        print(queue.queue[i], end=" ")
+    print()
+
+if __name__ == "__main__":
+    main()
+
